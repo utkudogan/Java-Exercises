adf7a2dc13c6cb7250b5ee3f551ff348cf8fa47a
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-02-08 23:33:00 +0300

    Maps, remove and replace methods

--- Collections-Overview/src/com/utkudogan/MapProgram.java
+++ Collections-Overview/src/com/utkudogan/MapProgram.java
@@ -19,10 +19,34 @@ public class MapProgram {
             languages.put("Java", "lan6");
         }
 
+
         System.out.println(languages.put("Java", "lan1"));
 
         for (String key: languages.keySet()) {
             System.out.println(key + ": " + languages.get(key));
         }
+
+        languages.remove("R");
+
+        if (languages.remove("Algol", "lan3")) {
+            System.out.println("Algol is removed");
+        } else {
+            System.out.println("nothing is removed");
+        }
+
+        if (languages.remove("Algol", "lan3")) {
+            System.out.println("Algol is removed");
+        } else {
+            System.out.println("nothing is removed");
+        }
+
+        languages.replace("Java", "langgg");
+
+        if (languages.replace("Java", "langgg", "lan1")) {
+            System.out.println("Java is replaced");
+        } else {
+            System.out.println("nothing is replaced");
+        }
+
     }
 }
