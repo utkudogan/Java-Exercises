252cefe856ef0ea76769557474cdaa77a0bc3b1e
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-02-14 00:07:31 +0300

    worked on layout

--- JavaFXToDoList/src/com/utkudogan/todolist/Controller.java
+++ JavaFXToDoList/src/com/utkudogan/todolist/Controller.java
@@ -1,13 +1,17 @@
 package com.utkudogan.todolist;
 
 import com.utkudogan.todolist.datamodel.ToDoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +20,10 @@ public class Controller {
     private ListView<ToDoItem> todoListView;
     @FXML
     private TextArea itemDetailTextArea;
+    @FXML
+    private Label deadLineLabel;
+
+
     private List<ToDoItem> toDoItems;
 
     public void initialize(){
@@ -37,17 +45,32 @@ public class Controller {
         toDoItems.add(item4);
         toDoItems.add(item5);
 
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ToDoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends ToDoItem> observableValue, ToDoItem toDoItem, ToDoItem t1) {
+                if (t1 != null){
+                    ToDoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadLineLabel.setText(df.format(item.getDeadLine()));
+                }
+            }
+        });
+
         todoListView.getItems().setAll(toDoItems);
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
     }
 
-    @FXML
-    public void handleClickListView(){
-        ToDoItem item = todoListView.getSelectionModel().getSelectedItem();
-        StringBuilder sb = new StringBuilder(item.getDetails());
-        sb.append("\n\n\n\n");
-        sb.append("Due: ");
-        sb.append(item.getDeadLine().toString());
-        itemDetailTextArea.setText(sb.toString());
-    }
+//    @FXML
+//    public void handleClickListView(){
+//        ToDoItem item = todoListView.getSelectionModel().getSelectedItem();
+//        itemDetailTextArea.setText(item.getDetails());
+//        deadLineLabel.setText(item.getDeadLine().toString());
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n");
+//        sb.append("Due: ");
+//        sb.append(item.getDeadLine().toString());
+//        itemDetailTextArea.setText(sb.toString());
+//    }
 }
