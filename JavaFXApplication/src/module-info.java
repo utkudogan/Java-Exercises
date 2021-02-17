633c6e6acec9a2c42cde7a4c669d40865d5550f8
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-02-18 00:53:22 +0300

    Linking to a web page

--- JavaFXApplication/src/module-info.java
+++ JavaFXApplication/src/module-info.java
@@ -1,5 +1,7 @@
 module JavaFXApplication {
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.web;
+    requires java.desktop;
     opens sample;
 }
--- JavaFXApplication/src/sample/Controller.java
+++ JavaFXApplication/src/sample/Controller.java
@@ -5,11 +5,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
+import java.awt.*;
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+
 
 public class Controller {
     @FXML
@@ -18,7 +24,8 @@ public class Controller {
     private Button button4;
     @FXML
     private GridPane gridPane;
-
+    @FXML
+    private WebView webView;
 
     public void initialize(){
         button4.setEffect(new DropShadow());
@@ -47,6 +54,7 @@ public class Controller {
                 new FileChooser.ExtensionFilter("All Files", "*.*")
         );
         //DirectoryChooser chooser = new DirectoryChooser();
+        //File file = chooser.showOpenDialog()Dialog(gridPane.getScene().getWindow());
         List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
         if (file == null) {
             for (int i=0;i<file.size();i++) {
@@ -56,4 +64,17 @@ public class Controller {
             System.out.println("cancelled");
         }
     }
+
+    @FXML
+    public void handleLinkClick(){
+        try{
+           Desktop.getDesktop().browse(new URI("www.javafx.com"));
+        }catch (Exception e){
+            e.printStackTrace();
+       }
+
+        WebEngine engine = webView.getEngine();
+        engine.load("htttp://www.javafx.com");
+    }
+
 }
