dcace10071b1501d485d36ab349311212549f0e5
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-02-27 22:56:33 +0300

    I/O Writing a .txt file

--- InputOutput/src/com/utkudogan/Locations.java
+++ InputOutput/src/com/utkudogan/Locations.java
@@ -1,13 +1,67 @@
 package com.utkudogan;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class Locations implements Map<Integer,Location> {
-    private static Map<Integer, Location> locations = new HashMap<>();
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("Locations.txt");
+            for (Location location: locations.values()) {
+                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+            }
+        }catch (IOException e){
+            System.out.println("Catch Block");
+            e.printStackTrace();
+        }finally {
+            System.out.println("finally block");
+            try {
+                if (locFile != null) {
+                    locFile.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static {
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest",tempExit));
+
+    }
     @Override
     public int size() {
         return locations.size();
@@ -20,27 +74,27 @@ public class Locations implements Map<Integer,Location> {
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return locations.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return locations.containsValue(value);
     }
 
     @Override
     public Location get(Object key) {
-        return null;
+        return locations.get(key);
     }
 
     @Override
     public Location put(Integer key, Location value) {
-        return null;
+        return locations.put(key, value);
     }
 
     @Override
     public Location remove(Object key) {
-        return null;
+        return locations.remove(key);
     }
 
     @Override
@@ -50,21 +104,22 @@ public class Locations implements Map<Integer,Location> {
 
     @Override
     public void clear() {
+        locations.clear();
 
     }
 
     @Override
     public Set<Integer> keySet() {
-        return null;
+        return locations.keySet();
     }
 
     @Override
     public Collection<Location> values() {
-        return null;
+        return locations.values();
     }
 
     @Override
     public Set<Entry<Integer, Location>> entrySet() {
-        return null;
+        return locations.entrySet();
     }
 }
